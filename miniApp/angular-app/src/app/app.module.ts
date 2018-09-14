e5482fb7e59887f0b0084f1ec034502e5baebdb1
Matt Cote <mwcote97@gmail.com>
2018-09-14 13:23:12 -0400

    db setup and basic routing

--- miniApp/angular-app/src/app/app.module.ts
+++ miniApp/angular-app/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '',      component: LoginComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- miniApp/app.js
+++ miniApp/app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
@@ -21,6 +24,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+
+var mongoDB = config.dbURL;
+mongoose.connect(mongoDB);
+
+//connecting to the database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("Connected to the DB");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +53,10 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+/*
+
+in angular-app: ng serve
+in miniApp: set DEBUG=miniApp:* & npm start
+*/
