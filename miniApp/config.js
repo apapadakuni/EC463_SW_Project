266d98a1d23260efeb6f43eb4c4bc593518681e6
Matt Cote <mwcote97@gmail.com>
2018-09-19 22:20:48 -0400

    added config

---
+++ miniApp/config.js
@@ -0,0 +1,6 @@
+//secret keys that need to be used to access the oauth and db services.
+module.exports = {
+    dbURL: 'mongodb://mwcote:ec463password@ds257732.mlab.com:57732/ec463_mini_project',
+    googleClientID: '314378448838-68jbiiuo9fk6hi75k5j9tcbo8tj9vetn.apps.googleusercontent.com',
+    googleClientSecret: 'btLCNvwiIHnHTKDQub2wv9Fn'
+}
